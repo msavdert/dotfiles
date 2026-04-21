4a1713cb24e3bbf8ff9784ec9f6faac3bc3bbdfb
msavdert <msavdert@users.noreply.github.com>
2026-04-21 10:19:22 -0400

    refactor: modularize plugin configurations by integrating them directly into lazy.nvim setup definitions

--- configs/nvim/lua/plugins/init.lua
+++ configs/nvim/lua/plugins/init.lua
@@ -15,36 +15,44 @@ vim.opt.rtp:prepend(lazypath)
 -- Define Plugins
 require("lazy").setup({
   -- UI & Essentials
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000, config = function() require("plugins.configs.others") end },
   { "nvim-lualine/lualine.nvim", dependencies = { "nvim-tree/nvim-web-devicons" } },
   { "nvim-tree/nvim-tree.lua", dependencies = { "nvim-tree/nvim-web-devicons" } },
   { "goolord/alpha-nvim" },
   { "lukas-reineke/indent-blankline.nvim", main = "ibl" },
 
   -- Navigation & Search
-  { "nvim-telescope/telescope.nvim", branch = "0.1.x", dependencies = { "nvim-lua/plenary.nvim" } },
+  { 
+    "nvim-telescope/telescope.nvim", 
+    branch = "0.1.x", 
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function() require("plugins.configs.telescope") end
+  },
 
   -- Treesitter
-  { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+  { 
+    "nvim-treesitter/nvim-treesitter", 
+    build = ":TSUpdate",
+    config = function() require("plugins.configs.treesitter") end
+  },
 
   -- LSP & Completion
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  "hrsh7th/nvim-cmp",
-  "hrsh7th/cmp-nvim-lsp",
-  "L3MON4D3/LuaSnip",
-  "saadparwaiz1/cmp_luasnip",
-  "rafamadriz/friendly-snippets",
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "rafamadriz/friendly-snippets",
+    },
+    config = function() require("plugins.configs.lsp") end
+  },
 
   -- Git & UX
   { "lewis6991/gitsigns.nvim" },
   { "folke/which-key.nvim" },
   { "numToStr/Comment.nvim" },
 })
-
--- Load specific plugin configurations after setup
-require("plugins.configs.others")
-require("plugins.configs.lsp")
-require("plugins.configs.telescope")
-require("plugins.configs.treesitter")
